7115ef2a6856c28faf5967e751f6abb244e186c1
i-ac <inge.ackerman@student.howest.be>
2025-01-23 20:00:15 +0100

    mobile  navigation with key accessibility and progressive enhancement

---
+++ src/js/navigation.js
@@ -0,0 +1,52 @@
+{
+    // code derived from classes and modified to fit the project
+    // source: https://codepen.io/simonvanherweghe/pen/xxoJyXW
+
+      const $navButton = document.querySelector('.main-nav__button');
+      const $navList = document.querySelector('.main-nav__list');
+      const $navLangList = document.querySelector('.lang-nav__list');
+      const $primaryButton = document.querySelector('.header__primary-button');
+      const $iconOpen = document.querySelector('.button__svg #menu_open');
+      const $iconClose = document.querySelector('.button__svg #menu_close');
+
+      // Function to toggle visibility of navigation elements
+      const toggleVisibility = (isOpen) => {
+          $navButton.setAttribute("aria-expanded", isOpen);
+          $iconOpen.classList.toggle("hidden", isOpen);
+          $iconClose.classList.toggle("hidden", !isOpen);
+          $navList.classList.toggle("hidden", !isOpen);
+          $navLangList.classList.toggle("hidden", !isOpen);
+          $primaryButton.classList.toggle("hidden", !isOpen);
+      }
+
+      const toggleNavigation = () => {
+          const isOpen = $navButton.getAttribute("aria-expanded") === "true";
+          toggleVisibility(!isOpen);
+      }
+
+      const handleBlur = (event) => {
+        if (!event.relatedTarget || !$primaryButton.contains(event.relatedTarget)) {
+          toggleVisibility(false);
+        }
+      }
+
+      // Initialize the mobile navigation with js
+      const init = () => {
+          $navButton.classList.remove('hidden');
+          toggleVisibility(false); // Start with navigation closed
+          $navButton.addEventListener("click", toggleNavigation);
+
+          // Add event to the last item in the list to trigger the disclosure to close if the user tabs out of the disclosure
+          $primaryButton.addEventListener("blur", handleBlur);
+
+          // Close the disclosure if a user presses the escape key
+          window.addEventListener("keyup", (e) => {
+              if (e.key === "Escape") {
+                  $navButton.focus();
+                  toggleVisibility(false);
+              }
+          });
+      }
+
+      init();
+  }
